53a31006bb6e26ba9e4b7ff39d3cbe58059fd535
Mrpradhanji <114905691+Mrpradhanji@users.noreply.github.com>
2024-07-09 15:37:00 +0530

    set up jwt tokens

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -37,3 +37,23 @@ export const register = async(req, res) => {
         res.status(500).json({error:error.message});
     }
 }
+
+//LOGGING IN
+export const login = async (req, res) =>{
+    try {
+        const { email, password }= req.body;      
+        const user = await User.findOne({email:email});
+        if(!user) return res.status(400).json({msg: "User does not exist."});
+        
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) return res.status(400).json({msg:'Invalid Crdentials. ' });
+
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET) 
+       //delete the pass so it does not shows on the frontend
+        delete user.password;
+        res.status(200).json( {token, user});
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+ 
--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import authRoutes from './routes/auth.js'
 import { register } from './controllers/auth.js';
 
 /* CONFIGURATIONS */
@@ -38,6 +39,10 @@ const upload = multer({ storage });
 
 //ROUTES WITH FILES
 app.post("/auth/register", upload.single("picture"), register);
+
+//ROUTES
+app.use("/auth", authRoutes);5
+
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
