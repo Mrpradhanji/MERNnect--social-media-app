4b180ad29c4450f0727904ede228562dd4273523
Mrpradhanji <114905691+Mrpradhanji@users.noreply.github.com>
2024-07-11 00:29:46 +0530

    done another routes

--- server/index.js
+++ server/index.js
@@ -11,7 +11,9 @@ import { fileURLToPath } from 'url';
 import authRoutes from './routes/auth.js'
 import { register } from './controllers/auth.js';
 import { verifyToken } from './middleware/auth.js';
-import userRoutes from './routes/users.js'
+import userRoutes from './routes/users.js' ;
+import postRoutests from './routes/posts.js';
+import { createPost} from './controllers/posts.js ';
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -41,11 +43,11 @@ const upload = multer({ storage });
 
 //ROUTES WITH FILES
 app.post("/auth/register", upload.single("picture"),verifyToken, register);
-
+app.post('/posts', verifyToken, upload.single('picture'), createPost);
 //ROUTES
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
-
+app.use("/posts", postRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
